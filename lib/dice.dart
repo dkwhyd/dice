000e0aa549c479b1c45c647fb81b67fc6711a3c1
dkwhyd <dikiwhyd99@gmail.com>
2024-03-26 09:49:45 +0700

    create dice

---
+++ lib/dice.dart
@@ -0,0 +1,22 @@
+import 'dart:math';
+
+class Dice {
+  static List animations = ['Set1', 'Set2', 'Set3', 'Set4', 'Set5', 'Set6'];
+
+  static getRandomNumber() {
+    var random = Random();
+    int num = random.nextInt(5) + 1;
+    return num;
+  }
+
+  static Map getRandomAnimation() {
+    var random = Random();
+    int num = random.nextInt(5);
+    Map result = {num: animations[num]};
+    return result;
+  }
+
+  static Future wait3seconds() {
+    return Future.delayed(const Duration(seconds: 1));
+  }
+}
